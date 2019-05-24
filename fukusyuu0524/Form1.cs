1e43d4885b2b0ab10c228109356b50ec80c6e784
irisa <iri18shu@gmail.com>
2019-05-24 13:27:37 +0900

    kaiketu

--- fukusyuu0524/Form1.cs
+++ fukusyuu0524/Form1.cs
@@ -29,21 +29,25 @@ namespace fukusyuu0524
         private void button1_Click(object sender, EventArgs e)
         {
             vy = -5;
+            vx = 0;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             vx = 5;
+            vy = 0;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             vx = -5;
+            vy = 0;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             vy = 5;
+            vx = 0;
         }
     }
 }
