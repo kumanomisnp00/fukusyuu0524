f5e1af10d9f49363a9bee8aa499d23ec09444eba
irisa <iri18shu@gmail.com>
2019-05-24 13:23:54 +0900

    nazo

--- fukusyuu0524/Form1.cs
+++ fukusyuu0524/Form1.cs
@@ -12,9 +12,38 @@ namespace fukusyuu0524
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vy = -5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vx = 5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vx = -5;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vy = 5;
         }
     }
 }
